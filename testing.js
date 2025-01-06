b039bdcd06fcce390e719cfb06947d5ada4962bc
pavan5 <73188509+pavan5sai@users.noreply.github.com>
2025-01-06 17:55:02 +0530

    Add files via upload

---
+++ testing.js
@@ -0,0 +1,7 @@
+document.getElementById('MyLink').addEventListener('click', function(event) {
+    // Prevent the default action (navigation)
+    event.preventDefault();
+
+    // Navigate to the new page using JavaScript
+    window.location.href = 'https://www.example.com';  // replace with your target URL
+});
