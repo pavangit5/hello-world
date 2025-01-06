2948cf5ac04c31be158a2b794b2dcf19cae8e919
pavangit5 <pavansaipasupuleti@outlook.com>
2025-01-06 17:57:20 +0530

    change

--- testing.js
+++ testing.js
@@ -5,3 +5,6 @@ document.getElementById('MyLink').addEventListener('click', function(event) {
     // Navigate to the new page using JavaScript
     window.location.href = 'https://www.example.com';  // replace with your target URL
 });
+
+
+hjvcbwducbqcubieqb
